3a430b301b6f13e9e0a5f2abc5caebb58e6d2f25
GIGANTOMACHY\victo <victormvgo@gmail.com>
2025-12-21 15:58:49 -0700

    Johnny Updates: *Pom.xml now includes validation dependency * Login page now simulates logins using input validation. * Created LoginController * Creted LoginModel
    Victor Updates:
    * Register now counts with input validation using bootstrap classes to simulate a registration
    * Updated bootstrap of login page

---
+++ src/main/java/com/gcu/inventory/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.gcu.inventory.controller;
+
+import com.gcu.inventory.model.LoginModel;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("")
+    public String display(Model model) {
+        model.addAttribute("loginModel", new LoginModel());
+        return "login";
+    }
+
+    @PostMapping("/doLogin")
+    public String doLogin(@Valid LoginModel loginModel,
+                          BindingResult bindingResult,
+                          Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "login";
+        }
+
+        return "redirect:/products";
+    }
+}
--- src/main/java/com/gcu/inventory/controller/ProductController.java
+++ src/main/java/com/gcu/inventory/controller/ProductController.java
@@ -2,16 +2,18 @@ package com.gcu.inventory.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("/products")
+    @GetMapping("")
     public String products() {
         return "products";
     }
 
-    @GetMapping("/products/create")
+    @GetMapping("/create")
     public String createProduct() {
         return "product-create";
     }
