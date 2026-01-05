f0c50902a9480838071efdd77b8ce5c8aa5da676
victormvgo@gmail.com <victormvgo@gmail.com>
2026-01-04 22:43:39 -0700

    Added login code done by johnny

--- src/main/java/com/gcu/inventory/controller/ProductController.java
+++ src/main/java/com/gcu/inventory/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/products")
+@Req                                                           
 public class ProductController {
 
     // Products list page
